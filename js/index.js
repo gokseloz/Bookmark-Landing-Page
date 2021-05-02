357b25d91c5b3620f8478fb3cf7360a402a23de1
Goksel Ozkazanc <goksel_ozkazanc_08@hotmail.com>
2021-05-03 00:25:27 +0200

    Add aria-controls and aria-selected ín features section

--- js/index.js
+++ js/index.js
@@ -7,23 +7,22 @@ let navLink = document.querySelectorAll(".header-nav-link")
 let navSocialLink = document.querySelectorAll(".nav-social-link")
 let login = document.querySelector(".header-login-link")
 
-if(window.innerWidth < 800){
+if (window.innerWidth < 800) {
     navLink.forEach(link => link.setAttribute("tabindex", "-1"))
     login.setAttribute("tabindex", "-1")
 }
 
 hamburgerIcon.addEventListener("click", () => {
     header.classList.toggle("nav-active")
-    body.classList.toggle("scrollDisabled") 
+    body.classList.toggle("scrollDisabled")
 
     hamburgerIcon.getAttribute("aria-expanded") === "false" ? hamburgerIcon.setAttribute("aria-expanded", "true") : hamburgerIcon.setAttribute("aria-expanded", "false")
     hamburgerIcon.getAttribute("aria-pressed") === "false" ? hamburgerIcon.setAttribute("aria-pressed", "true") : hamburgerIcon.setAttribute("aria-pressed", "false")
 
-    if(header.classList.contains("nav-active")) {
+    if (header.classList.contains("nav-active")) {
         navLink.forEach(link => link.setAttribute("tabindex", "0"))
         login.setAttribute("tabindex", "0")
-    }
-    else {
+    } else {
         navLink.forEach(link => link.setAttribute("tabindex", "-1"))
         login.setAttribute("tabindex", "-1")
     }
@@ -44,24 +43,26 @@ questions.forEach((question) => {
 // --------------------
 // * FEATURES SECTION *
 // --------------------
-let feature = [
-    {
+let feature = [{
         imgSrc: './images/illustration-features-tab-1.svg',
         heading: 'Bookmark in one click',
         desc: 'Organize your bookmarks however you like. Our simple drag-and-drop interface gives you complete control over how you manage your favourite sites.',
-        altTxt: 'control panel'
+        altTxt: 'control panel',
+        id: "simple"
     },
     {
         imgSrc: './images/illustration-features-tab-2.svg',
         heading: 'Intelligent search',
         desc: 'Our powerful search feature will help you find saved sites in no time at all. No need to trawl through all of your bookmarks.',
-        altTxt: 'quick search'
+        altTxt: 'quick search',
+        id: "quick"
     },
     {
         imgSrc: './images/illustration-features-tab-3.svg',
         heading: 'Share your bookmarks',
         desc: 'Easily share your bookmarks and collections with others. Create a shareable link that you can send at the click of a button.',
-        altTxt: 'communicate with others'
+        altTxt: 'communicate with others',
+        id: "easy"
     },
 ]
 
@@ -73,10 +74,12 @@ let featureDesc = document.querySelector(".feature-desc")
 let tabButtons = [...document.querySelectorAll(".tab")]
 
 function changeTab(index) {
-    tabButtons.forEach((tab) => {
+    tabButtons.forEach(tab => {
         tab.classList.remove("active")
+        tabButtons.forEach(tab => tab.setAttribute("aria-selected", "false"))
     })
     tabButtons[index].classList.add("active")
+    tabButtons[index].setAttribute("aria-selected", "true")
 
     featureImgWrapper.classList.add("animAdded")
     featureTextWrapper.classList.add("animAdded")
@@ -90,15 +93,17 @@ function changeTab(index) {
         featureImg.src = feature[index].imgSrc;
         featureImg.alt = feature[index].altTxt;
         featureHeading.textContent = feature[index].heading;
+        featureHeading.id = feature[index].id
         featureDesc.textContent = feature[index].desc;
     }, 800);
 }
 
 tabButtons.forEach((tab) => {
-    tab.addEventListener("click", function () {
+    tab.addEventListener("click", () => {
         changeTab(tabButtons.indexOf(tab))
+        keydownEventListener()
     })
-    tab.addEventListener("keyup", function (e) {
+    tab.addEventListener("keyup", (e) => {
         if (e.keyCode === 13) {
             e.preventDefault()
             changeTab(tabButtons.indexOf(tab))
